8ab3a98555ffe589f207785e842429934316bc58
Damira K <khamzinadamira@gmail.com>
2023-05-04 21:05:26 +1000

    came up with quiz questions and imported them into the python file

---
+++ quiz_contents.py
@@ -0,0 +1,17 @@
+import csv 
+
+movies = open("movie_questions.csv", "r")
+datareader = csv.reader(movies)
+questions = []
+for row in datareader:
+  questions.append(row)
+num_questions = len(questions) -1
+movies.close()
+
+literature = open("literature_questions.csv", "r")
+datareader = csv.reader(literature)
+literature_q = []
+for row in datareader:
+  literature_q.append(row)
+num_questions_literature = len(literature_q) -1
+literature.close()
