e46fd16b1b79daed7d428cc4e54eba7719921954
Damira K <khamzinadamira@gmail.com>
2023-05-09 16:33:58 +1000

    added a progress bar and made some changes to the loop

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 ### Link to my repository: https://github.com/Mira-cs/terminal_application
+### A list of features of the terminal application (Quiz):
+- **Being able to choose topic and one options out of four presented**  
+I came up with three topics for the quiz as well as 9 questions for each topic as well as 4 possible answers. In the beginning users are able to choose one topic out of three.Then, according to the answer they provide, they will be asked 9 questions in a certain order. I stored questions and options in the CSV file,which is then imported into the python program.  
+The quiz itself is executed with the help of "while" and "for" loops
+"While" will keep looping as long as the answers provided are valid. "For" loop will execute as long as there are questions available.
+- **Score count**   
+The program counts the number of questions the user did right, providing the overall score in the end.In case of incorrect input, the program will output the correct answer as well as the score.
+- **Hints**  
+User can also request a hint in case they are struggling.I created a separate hint file that is then imported into the python file, just like the quiz questions.
+
 
 
--- quiz_contents.py
+++ quiz_contents.py
@@ -1,6 +1,6 @@
 import csv #importing files with quiz questions
 from stringcolor import cs #importing one module from stringcolor, to change the color of text
-import random #to be able to generate random question from chosen topic
+from progress.bar import FillingSquaresBar #to show how much progress user made through the quiz
 
 movies = open("movie_questions.csv", "r")
 datareader = csv.reader(movies)
@@ -15,7 +15,7 @@ datareader = csv.reader(literature)
 literature_q = []
 for row in datareader:
   literature_q.append(row)
-num_questions_literature = len(literature_q) - 1
+num_questions = len(questions) - 1
 literature.close()
 
 art = open("art_questions.csv", "r")
@@ -23,56 +23,58 @@ datareader = csv.reader(art)
 art_q = []
 for row in datareader:
   art_q.append(row)
-num_questions_art = len(art_q) - 1
+num_questions = len(questions) - 1
 art.close()
-
+bar = FillingSquaresBar('Processing', max=num_questions)
 #  Outputs a welcome message and lets the user decide the topic
-print(cs("Hello! Welcome to the Multiple Choice Quiz!","blue"))
+print(cs("\nHello! Welcome to the Multiple Choice Quiz!","blue"))
 count = 0
 valid_options = ["a","b","c","d","A","B","C","D"]
-q_number = 1
 while True:
-  choose = input(cs("\nChoose from the topics available by typing movies,literature or art and anything else to exit: \n","orchid")).lower()
-  while choose == "movies" or choose == "literature" or choose == "art":
+  choose = input(cs("\nChoose from the topics available by typing movies,literature or art or anything else to exit: \n","purple")).lower()
+  if choose == "movies" or choose == "literature" or choose == "art":
     if choose == "movies":
       for i in range(1,10):
-            print("Question ", i , "-", questions[q_number][0])
-            print("A", questions[q_number][1])
-            print("B ", questions[q_number][2])
-            print("C ", questions[q_number][3])
-            print("D ", questions[q_number][4])
+            print("\nQuestion ",i , "-", questions[i][0])
+            print("A", questions[i][1])
+            print("B ", questions[i][2])
+            print("C ", questions[i][3])
+            print("D ", questions[i][4])
             answer = input("Answer (A to D), or a 'hint': ").upper()
-            if answer == questions[q_number][5]:
+            if answer == questions[i][5]:
               count += 1
               print(cs(f"\n Correct,you have {count} correct answer(s)\n","green"))
-              q_number += 1
+              if i < num_questions:
+                bar.next(1)
             elif answer in valid_options:
-              print(cs(f"\n Wrong,you have {count} correct answer(s)\n", "red"))
-              q_number += 1 
+              print(cs(f"\n Wrong,the correct answer is {questions[i][5]}. You have {count} correct answer(s)\n", "red"))
+              if i < num_questions:
+                bar.next(1)
             else:
               print(cs("Please provide a valid answer","yellow"))
-              break
     elif choose == "literature":
         for i in range(1,10):
-            print("Question ", i , "-", literature_q[q_number][0])
-            print("A", literature_q[q_number][1])
-            print("B ", literature_q[q_number][2])
-            print("C ", literature_q[q_number][3])
-            print("D ", literature_q[q_number][4])
+            print("\nQuestion ", i , "-", literature_q[i][0])
+            print("A", literature_q[i][1])
+            print("B ", literature_q[i][2])
+            print("C ", literature_q[i][3])
+            print("D ", literature_q[i][4])
             answer = input("Answer (A to D), or a 'hint': ").upper()
-            if answer == literature_q[q_number][5]:
+            if answer == literature_q[i][5]:
               count += 1
               print(cs(f"\n Correct,you have {count} correct answer(s)\n","green"))
-              q_number += 1
+              if i <num_questions:
+                bar.next(1)
             elif answer in valid_options:
-              print(cs(f"\n Wrong,you have {count} correct answer(s)\n", "red"))
-              q_number += 1 
+              print(cs(f"\n Wrong,the correct answer is {literature_q[i][5]}. You have {count} correct answer(s)\n", "red"))
+              if i < num_questions:
+                bar.next(1)
             else:
               print(cs("Please provide a valid answer","yellow"))
               break
     elif choose == "art":
         for i in range(1,10):
-            print("Question ", i , "-", art_q[i][0])
+            print("\nQuestion ", i , "-", art_q[i][0])
             print("A", art_q[i][1])
             print("B ", art_q[i][2])
             print("C ", art_q[i][3])
@@ -81,25 +83,29 @@ while True:
             if answer == art_q[i][5]:
               count += 1
               print(cs(f"\n Correct,you have {count} correct answer(s)\n","green"))
+              if i <num_questions:
+                bar.next(1)
             elif answer in valid_options:
-              print(cs(f"\n Wrong,you have {count} correct answer(s)\n", "red"))
+              print(cs(f"\n Wrong,the correct answer is {art_q[i][5]}. You have {count} correct answer(s)\n", "red"))
+              if i < num_questions:
+                bar.next(1)
             else:
               print(cs("Please provide a valid answer","yellow"))
-              break
-    if count >= 8:
-      print(cs(f"\nWow,good job, you have {count} correct answer(s) out of {i} questions!\n", "purple"))
-      yes_no = input("Would you like to take quiz again?: ")
-      if yes_no == "yes":
-        continue
-      else:
-        break
-    elif count <= 7:
-      print(cs(f"\nYou have {count} correct answer(s) out of {i}.Don't worry, you can always play again!\n", "purple"))
-      yes_no = input("Would you like to take quiz again?: ")
-      if yes_no == "yes":
-        continue
-      else:
-        break
+    # if count >= 8:
+    #   print(cs(f"\nWow,good job, you have {count} correct answer(s) out of {i} questions!\n", "purple"))
+    #   yes_no = input("Would you like to take quiz again?: ")
+    #   if yes_no == "yes":
+    #     continue
+    #   else:
+    #     break
+    # elif count <= 7:
+    #   print(cs(f"\nYou have {count} correct answer(s) out of {i}.Don't worry, you can always play again!\n", "purple"))
+    #   yes_no = input("Would you like to take quiz again?: ")
+    #   if yes_no == "yes":
+    #     continue
+    #   else:
+    #     break
   else:
     print(cs("\nPlease provide a valid answer\n","yellow"))
     break
+
