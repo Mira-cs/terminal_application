9f3016c802e581d2357f9391aa00c0d9c1cce6ef
Damira K <khamzinadamira@gmail.com>
2023-05-13 03:16:58 +1000

    created a test for the progressbar function

--- test_quiz.py
+++ test_quiz.py
@@ -1,4 +1,4 @@
-from quiz_functions import is_valid_input
+from quiz_functions import is_valid_input,quiz_progressbar
 from progress.bar import FillingCirclesBar
 # testing to see if the program matches the input to the valid_options list items and returns the correct result
 def test_is_valid_input(monkeypatch):
@@ -7,12 +7,20 @@ def test_is_valid_input(monkeypatch):
 
   monkeypatch.setattr("builtins.input", lambda _: "invalid")
   assert is_valid_input("Please choose one topic (movies, art, literature, or exit): ", "yellow") == False
-def test_quiz_progress_bar():
-    num_questions = 5
-    bar = FillingCirclesBar('Progress', max=num_questions - 1)
-    for i in range(num_questions):
-        # Simulate answering a question
-        bar.next(1)
-    bar.finish()
-    assert bar.index == num_questions - 1 
+#testing if the progressbar works as expected
+def test_quiz_progressbar():
+    num_questions = 10
+    bar = FillingCirclesBar("Progress", max=num_questions)
+    
+    # Test that the bar is empty at the beginning
+    assert bar.index == 0
+    
+    # Test that the bar advances after calling next()
+    bar.next()
+    assert bar.index == 1
+    
+    # Test that the bar is full after calling next() num_questions times
+    for i in range(num_questions - 1):
+        bar.next()
+    assert bar.index == num_questions
   
